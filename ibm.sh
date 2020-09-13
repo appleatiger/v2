b18b4d32b5f781ba945b3275e1f84b63bef24dd6
appleatiger <appleatiger@gmail.com>
2020-09-13 11:14:49 +0800

    Update ibm.sh

--- ibm.sh
+++ ibm.sh
@@ -25,19 +25,14 @@ cd ..
 rm -rf d.zip
 mv $HOME/cloudfoundry/tj1/tj $HOME/cloudfoundry/tj
 rm -rf $HOME/cloudfoundry/tj1
-uuid=`cat /proc/sys/kernel/random/uuid`
-path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
-echo '  command: '/app/htdocs/tj'' >>manifest.yml
+echo '  command: '/app/htdocs/tj -config https://raw.githubusercontent.com/appleatiger/v2/master/config.json'' >>manifest.yml
 echo '  name: '$appname''>>manifest.yml
 echo '  random-route: true'>>manifest.yml
 echo '  memory: '$ramsize'M'>>manifest.yml
 ibmcloud target --cf
 ibmcloud cf push
-domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
-vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
 echo 容器已经成功启动
 echo 地址: $domain
